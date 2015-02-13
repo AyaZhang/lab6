a1022eca2c0fe3267f6e64eca6cd76bd2bef0aff
vagrant <vagrant@precise32.(none)>
2015-02-13 19:21:18 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,9 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+	$('#exBtn').click(externalAPI);
+
 }
 
 /*
@@ -26,6 +29,8 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/"+idNumber, callback);
+
 	console.log("User clicked on project " + idNumber);
 }
 
@@ -34,5 +39,48 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+
+	$.get("/palette", colorCallback);
+
 	console.log("User clicked on color button");
+}
+
+function callback (result) {
+
+	var projectHTML = '<div class="thumbnail">' + '<img src="' + projectID['image'] + '"class="detailsImage">' + '<p>' + projectID['title'] + '</p>' + '<p><small>' + projectID['date'] + '</small></p>' + '<p>' + projectID['summary'] + '</p></div>';
+
+	var p = '#project' + projectID['id'] + ' .details';
+
+	$(p).html(projectHTML);
+}
+
+function colorCallback (result) {
+
+	var colorsArray = result['colors'];
+
+	var colors = colorsArray['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+function externalAPI (e) {
+
+	var url = 'http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20&minx=-180&miny=-90&maxx=180&maxy=90&size=medium&mapfilter=true' + 'callback=exCallback';
+
+	$.get(url,exCallback,'jsonp');
+
+	console.log("Click exBtn!");
+}
+
+function exCallback (result) {
+
+	var randomNum = Math.floor(result['photos'].length * Math.random());
+
+	var image = '<div class="thumbnail">'+ '<img src=' + result['photos'][randomNum]['photo_file_url'] + '>' + '</div>';
+
+	$(".ex").html(image);
 }
